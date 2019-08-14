88dc37ba7d47679b01a378dc93c3db4f701cd09c
trnkv <ttrnkv75@yandex.ru>
2019-08-14 16:54:42 +0300

    Fixed function to view excursions

--- app/delete.py
+++ app/delete.py
@@ -5,3 +5,11 @@ from .models import Excursion, Area, Facility, Organizator, Incharge, Guide
 class ExcursionDelete(DeleteView):
     model = Excursion
     success_url = reverse_lazy('schedule')
+
+    #def get_queryset(self):
+         #queryset = super().get_queryset()
+         #return queryset.filter(recipient = self.request.user)
+
+    def delete(self,*args,**kwargs):
+    	#messages.success(self.request,'Excursion Deleted')
+    	return super().delete(*args,**kwargs)
